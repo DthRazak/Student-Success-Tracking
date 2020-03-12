a232416fe0a65c9cb7ab17a4f4c7d83db1e639bb
Володимир Мільчановський <vovamilchan@gmail.com>
2020-03-12 10:06:46 +0200

    Added DbContext

---
+++ src/SST.Persistence/SSTDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SST.Application.Common.Interfaces;
+using SST.Domain.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Persistence
+{
+    public class SSTDbContext : DbContext, ISSTDbContext
+    {
+        public SSTDbContext(DbContextOptions<SSTDbContext> options)
+            :base(options)
+        {}
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Lector> Lectors { get; set; }
+
+        public DbSet<Request> Requests { get; set; }
+
+        public DbSet<Subject> Subjects { get; set; }
+
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+
+        public DbSet<Grade> Grades { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO : apply configurations
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
