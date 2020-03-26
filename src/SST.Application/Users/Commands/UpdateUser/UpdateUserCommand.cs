36773621fd089fa6dbc1ca62f4aa31b234cb9da6
Calvados0806 <flamaster0807@gmail.com>
2020-03-26 10:25:54 +0200

    implemented UpdateUser command

---
+++ src/SST.Application/Users/Commands/UpdateUser/UpdateUserCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace SST.Application.Users.Commands.UpdateUser
+{
+    class UpdateUserCommand : IRequest
+    {
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public bool? IsAdmin { get; set; }
+    }
+}
---
+++ src/SST.Application/Users/Commands/UpdateUser/UpdateUserCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using SST.Application.Common.Interfaces;
+using SST.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Application.Users.Commands.UpdateUser
+{
+    class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, Unit>
+    {
+        private readonly ISSTDbContext _context;
+
+        public UpdateUserCommandHandler(ISSTDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UpdateUserCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Users
+                .FindAsync(request.Email);
+
+            if (entity == null)
+                throw new ArgumentException($"User with Email({request.Email}) does not exists!");
+
+            if (request.PasswordHash != null)
+                entity.PasswordHash = request.PasswordHash;
+            if (request.IsAdmin != null)
+                entity.IsAdmin = (bool)request.IsAdmin;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
