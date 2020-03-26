64384d37c1cb1654663036520adeceac1aefc977
Calvados0806 <flamaster0807@gmail.com>
2020-03-26 09:58:47 +0200

    implemented CreateUser command

---
+++ src/SST.Application/Students/Commands/CreateStudent/CreateStudentCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using SST.Application.Common.Interfaces;
+using SST.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Application.Students.Commands.CreateStudent
+{
+    class CreateStudentCommandHandler : IRequestHandler<CreateStudentCommand, int>
+    {
+        private readonly ISSTDbContext _context;
+
+        public CreateStudentCommandHandler(ISSTDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateStudentCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Student
+            {
+                Id = request.Id,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Group = request.Group,
+                UserRef = request.UserRef
+            };
+
+            _context.Students.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
