08bb2de3a3bdb65922c85c9352edb19f9e9c7561
Calvados0806 <flamaster0807@gmail.com>
2020-03-26 10:50:04 +0200

    implemented DeleteRequest command

---
+++ src/SST.Application/Requests/Commands/DeleteRequest/DeleteRequestCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SST.Application.Requests.Commands.DeleteRequest
+{
+    class DeleteRequestCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/SST.Application/Requests/Commands/DeleteRequest/DeleteRequestCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using SST.Application.Common.Interfaces;
+using SST.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Application.Requests.Commands.DeleteRequest
+{
+    class DeleteRequestCommandHandler : IRequestHandler<DeleteRequestCommand, Unit>
+    {
+        private readonly ISSTDbContext _context;
+
+        public DeleteRequestCommandHandler(ISSTDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteRequestCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Requests
+                .FindAsync(request.Id);
+
+            if (entity == null)
+                throw new ArgumentException($"Request with Id({request.Id}) does not exists!");
+
+            _context.Requests.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
