5d2a580701d55ec12d97c7f366f84c585ff4189e
Calvados0806 <flamaster0807@gmail.com>
2020-03-26 10:37:36 +0200

    implemented CreateLector command

---
+++ src/SST.Application/Lectors/Commands/CreateLector/CreateLectorCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using SST.Application.Common.Interfaces;
+using SST.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Application.Lectors.Commands.CreateLector
+{
+    class CreateLectorCommandHandler : IRequestHandler<CreateLectorCommand, int>
+    {
+        private readonly ISSTDbContext _context;
+
+        public CreateLectorCommandHandler(ISSTDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateLectorCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new Lector
+            {
+                Id = request.Id,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                AcademicStatus = request.AcademicStatus,
+                UserRef = request.UserRef
+            };
+
+            _context.Lectors.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
