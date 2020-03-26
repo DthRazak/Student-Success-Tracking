ef2a159cbc73937e2d91e6c7d91330106f4cba28
Calvados0806 <flamaster0807@gmail.com>
2020-03-26 10:47:59 +0200

    implemented UpdateRequest command

---
+++ src/SST.Application/Requests/Commands/UpdateRequest/UpdateRequestCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+
+namespace SST.Application.Requests.Commands.UpdateRequest
+{
+    class UpdateRequestCommand : IRequest
+    {
+        public int Id { get; set; }
+
+        public bool? IsApproved { get; set; }
+
+        public DateTime? CreationDate { get; set; }
+
+        public string UserRef { get; set; }
+    }
+}
