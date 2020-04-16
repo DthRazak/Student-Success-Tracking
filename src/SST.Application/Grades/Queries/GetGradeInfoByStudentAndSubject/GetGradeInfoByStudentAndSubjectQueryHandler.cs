cb5d545e550f290fb52e297502b61ac403e91115
Володимир Мільчановський <vovamilchan@gmail.com>
2020-04-16 20:21:03 +0300

    Added GetGradeInfoByStudentAndSubjectQuery

---
+++ src/SST.Application/Grades/Queries/GetGradeInfoByStudentAndSubject/GetGradeInfoByStudentAndSubjectQueryHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SST.Application.Common.Interfaces;
+
+namespace SST.Application.Grades.Queries.GetGradeInfoByStudentAndSubject
+{
+    public class GetGradeInfoByStudentAndSubjectQueryHandler : IRequestHandler<GetGradeInfoByStudentAndSubjectQuery, GradesInfoVm>
+    {
+        private readonly ISSTDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetGradeInfoByStudentAndSubjectQueryHandler(ISSTDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<GradesInfoVm> Handle(GetGradeInfoByStudentAndSubjectQuery request, CancellationToken cancellationToken)
+        {
+            var group = (await _context.Students.FindAsync(request.StudentId)).Group;
+
+            var gradesInfos = await _context.StudentSubjects
+               .Where(x => (x.Student.Group == group) && (x.Subject.Id == request.SubjectId))
+               .ProjectTo<GradesInfoDto>(_mapper.ConfigurationProvider)
+               .ToListAsync(cancellationToken);
+
+            var dateSet = new HashSet<DateTime>();
+            foreach (var gradesInfo in gradesInfos)
+            {
+                foreach (var date in gradesInfo.Marks.Keys)
+                {
+                    dateSet.Add(date);
+                }
+            }
+
+            var dates = dateSet.ToList();
+            dates.Sort();
+
+            var vm = new GradesInfoVm
+            {
+                Dates = dates,
+                GradesInfos = gradesInfos
+            };
+
+            return vm;
+        }
+    }
+}
---
+++ src/SST.Application/Grades/Queries/GetGradeInfoByStudentAndSubject/GradesInfoVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SST.Application.Grades.Queries.GetGradeInfoByStudentAndSubject
+{
+    public class GradesInfoVm
+    {
+        public IList<DateTime> Dates { get; set; }
+
+        public IList<GradesInfoDto> GradesInfos { get; set; }
+    }
+}
