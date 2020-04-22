89ec4a683cf8588b632a8e74795359c85e89870d
Володимир Мільчановський <vovamilchan@gmail.com>
2020-04-22 21:54:04 +0300

    Fixed Student queries

--- src/SST.Application/Students/Queries/GetStudents/StudentDto.cs
+++ src/SST.Application/Students/Queries/GetStudents/StudentDto.cs
@@ -17,9 +17,10 @@ namespace SST.Application.Students.Queries.GetStudents
         public string Email { get; set; }
 
         public void Mapping(Profile profile)
-        { 
-            profile.CreateMap< Student, StudentDto > ()
-                  .ForMember(x => x.Email, y => y.MapFrom(z => z.UserRef));
+        {
+            profile.CreateMap<Student, StudentDto>()
+                  .ForMember(x => x.Email, y => y.MapFrom(z => z.UserRef))
+                  .ForMember(x => x.Group, y => y.MapFrom(z => z.Group.Name));
         }
     }
 }
