1e42db2d76f242afdb935450a26a674015a55d5b
hurska <nadiagurska@gmail.com>
2020-04-24 19:40:54 +0300

    created queries

---
+++ src/SST.Application/Groups/Queries/GetFaculties/GetFacultiesQuereHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace SST.Application.Groups.Queries.GetFaculties
+{
+    public class GetFacultiesQueryHandler : IRequestHandler<GetFacultiesQuery, FacultyListVm>
+    {
+        public async Task<FacultyListVm> Handle(GetFacultiesQuery request, CancellationToken cancellationToken)
+        {
+            var faculties = new List<string>
+            {
+                "Факультет прикладної математики та інформатики",
+                "Біологічний факультет",
+                "Географічний факультет",
+                "Геологічний факультет",
+                "Економічний факультет",
+                "Факультет електроніки та комп’ютерних технологій",
+                "Факультет журналістики",
+                "Факультет іноземних мов",
+                "Історичний факультет",
+                "Факультет культури і мистецтв",
+                "Механіко-математичний факультет",
+                "Факультет міжнародних відносин",
+                "Факультет педагогічної освіти",
+                "Факультет управління фінансами та бізнесу",
+                "Фізичний факультет",
+                "Філологічний факультет",
+                "Філософський факультет",
+                "Хімічний факультет",
+                "Юридичний факультет",
+            };
+
+            return new FacultyListVm { Faculties = faculties };
+        }
+    }
+}
---
+++ src/SST.Application/Groups/Queries/GetFaculties/GetFacultiesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SST.Application.Groups.Queries.GetFaculties
+{
+    public class GetFacultiesQuery : IRequest<FacultyListVm>
+    {
+    }
+}
+
