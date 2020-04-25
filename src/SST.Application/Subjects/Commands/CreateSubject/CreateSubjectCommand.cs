be23ab858534dd40df82143d8fbf75ef2046619a
Calvados0806 <flamaster0807@gmail.com>
2020-04-25 16:13:23 +0300

    implemented CreateSubject command

---
+++ src/SST.Application/Subjects/Commands/CreateSubject/CreateSubjectCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace SST.Application.Subjects.Commands.CreateSubject
+{
+    public class CreateSubjectCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+
+        public int LectorId { get; set; }
+    }
+}
---
+++ src/SST.Application/Subjects/Commands/CreateSubject/CreateSubjectCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using SST.Application.Common.Interfaces;
+using SST.Domain.Entities;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Application.Subjects.Commands.CreateSubject
+{
+    public class CreateSubjectCommandHandler : IRequestHandler<CreateSubjectCommand, int>
+    {
+        private readonly ISSTDbContext _context;
+
+        public CreateSubjectCommandHandler(ISSTDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateSubjectCommand request, CancellationToken cancellationToken)
+        {
+            var existing = _context.Subjects
+                .FirstOrDefault(s => s.Name == request.Name && s.LectorRef == request.LectorId);
+
+            if (existing != null)
+            {
+                return existing.Id;
+            }
+
+            var entity = new Subject
+            {
+                Name = request.Name,
+                LectorRef = request.LectorId
+            };
+
+            _context.Subjects.Add(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return entity.Id;
+        }
+    }
+}
