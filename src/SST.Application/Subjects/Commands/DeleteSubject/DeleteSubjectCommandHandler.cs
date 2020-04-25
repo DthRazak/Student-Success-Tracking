f41b81caa75611f24f5111f7589e256b36324538
Calvados0806 <flamaster0807@gmail.com>
2020-04-25 15:59:54 +0300

    implemented DeleteSubject command

---
+++ src/SST.Application/Subjects/Commands/DeleteSubject/DeleteSubjectCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using SST.Application.Common.Interfaces;
+using SST.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Application.Subjects.Commands.DeleteSubject
+{
+    public class DeleteSubjectCommandHandler : IRequestHandler<DeleteSubjectCommand, Unit>
+    {
+        private readonly ISSTDbContext _context;
+
+        public DeleteSubjectCommandHandler(ISSTDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteSubjectCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Subjects
+                .FindAsync(request.Id);
+
+            if (entity == null)
+                throw new ArgumentException($"Subject with Id({request.Id}) does not exists!");
+
+            _context.Subjects.Remove(entity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
