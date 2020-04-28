5bf57b6216610376986c5819bcaa9462078803e5
MSH63ack <sh.martusya@gmail.com>
2020-04-28 23:08:42 +0300

    Initially сreated LectorController, LectorLayout and Info page

---
+++ src/SST.WebUI/Controllers/LectorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SST.WebUI.Controllers
+{
+	[Authorize(Policy = "Lector")]
+	public class LectorController : Controller
+	{
+		private readonly ILogger<LectorController> _logger;
+		private readonly IMediator _mediator;
+
+		public LectorController(ILogger<LectorController> logger, IMediator mediator)
+		{
+			_logger = logger;
+			_mediator = mediator;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Info()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Subjects()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Gradebook()
+		{
+			return View();
+		}
+	}
+}
