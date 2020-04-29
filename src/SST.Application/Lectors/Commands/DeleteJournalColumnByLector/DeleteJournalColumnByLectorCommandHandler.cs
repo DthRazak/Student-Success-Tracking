f8c38fcbd30a270e5c660175b3ca99f548a5a71b
Calvados0806 <flamaster0807@gmail.com>
2020-04-29 22:42:31 +0300

    added DeleteJournalColumnByLector command

---
+++ src/SST.Application/Lectors/Commands/DeleteJournalColumnByLector/DeleteJournalColumnByLectorCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using SST.Application.Common.Interfaces;
+using SST.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Application.Lectors.Commands.DeleteJournalColumnByLector
+{
+    public class DeleteJournalColumnByLectorCommandHandler : IRequestHandler<DeleteJournalColumnByLectorCommand, Unit>
+    {
+        private readonly ISSTDbContext _context;
+
+        public DeleteJournalColumnByLectorCommandHandler(ISSTDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteJournalColumnByLectorCommand request, CancellationToken cancellationToken)
+        {
+            var journalColEnt = await _context.JournalColumns
+                .FindAsync(request.JournalColumnId);
+
+            if (journalColEnt != null)
+            {
+                if (journalColEnt.GroupSubject.Subject.LectorRef != request.LectorId)
+                {
+                    throw new Exception($"Actual LectorId({journalColEnt.GroupSubject.Subject.LectorRef}) isn't compatible with given({request.LectorId}");
+                }
+
+                _context.JournalColumns.Remove(journalColEnt);
+
+                await _context.SaveChangesAsync(cancellationToken);
+
+                return Unit.Value;
+            }
+            else
+            {
+                throw new Exception($"JournalColumn with id({request.JournalColumnId}) doesn't exists");
+            }
+        }
+    }
+}
