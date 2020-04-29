2eab4ec39b323326744b55f20b53be13c76cf605
MSH63ack <sh.martusya@gmail.com>
2020-04-29 20:32:10 +0300

    Finished Info page for the Lector

--- src/SST.WebUI/Controllers/LectorController.cs
+++ src/SST.WebUI/Controllers/LectorController.cs
@@ -7,42 +7,47 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using SST.Application.Subjects.Queries.GetSubjectsByLector;
+using SST.Application.Lectors.Queries.GetLector;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SST.WebUI.Controllers
 {
-	[Authorize(Policy = "Lector")]
-	public class LectorController : Controller
-	{
-		private readonly ILogger<LectorController> _logger;
-		private readonly IMediator _mediator;
+    [Authorize(Policy = "Lector")]
+    public class LectorController : Controller
+    {
+        private readonly ILogger<LectorController> _logger;
+        private readonly IMediator _mediator;
 
-		public LectorController(ILogger<LectorController> logger, IMediator mediator)
-		{
-			_logger = logger;
-			_mediator = mediator;
-		}
+        public LectorController(ILogger<LectorController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
 
-		[HttpGet]
-		public async Task<IActionResult> Info()
-		{
-			return View();
-		}
+        [HttpGet]
+        public async Task<IActionResult> Info()
+        {
+            var id = int.Parse(User.Claims.First(x => x.Type == "SST-ID").Value);
 
-		[HttpGet]
-		public async Task<IActionResult> Subjects()
-		{
-			var id = int.Parse(User.Claims.First(x => x.Type == "SST-ID").Value);
+            var model = await _mediator.Send(new GetLectorQuery { LectorId = id });
 
-			var model = await _mediator.Send(new GetSubjectsByLectorQuery { LectorId = id });
+            return View(model);
+        }
 
-			return View(model);
-		}
+        [HttpGet]
+        public async Task<IActionResult> Subjects()
+        {
+            var id = int.Parse(User.Claims.First(x => x.Type == "SST-ID").Value);
 
-		[HttpGet]
-		public async Task<IActionResult> Gradebook()
-		{
-			return View();
-		}
-	}
+            var model = await _mediator.Send(new GetSubjectsByLectorQuery { LectorId = id });
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Gradebook()
+        {
+            return View();
+        }
+    }
 }
