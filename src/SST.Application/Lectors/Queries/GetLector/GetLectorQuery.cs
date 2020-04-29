57ed418bd9066ddf3b347919b4514f6bf9466dd6
Володимир Мільчановський <vovamilchan@gmail.com>
2020-04-29 15:57:38 +0300

    Added GetLectorQuery

---
+++ src/SST.Application/Lectors/Queries/GetLector/GetLectorQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SST.Application.Lectors.Queries.GetLector
+{
+    public class GetLectorQuery : IRequest<LectorVm>
+    {
+        public int LectorId { get; set; }
+    }
+}
---
+++ src/SST.Application/Lectors/Queries/GetLector/GetLectorQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SST.Application.Common.Interfaces;
+
+namespace SST.Application.Lectors.Queries.GetLector
+{
+    public class GetLectorQueryHandler : IRequestHandler<GetLectorQuery, LectorVm>
+    {
+        private readonly ISSTDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetLectorQueryHandler(ISSTDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<LectorVm> Handle(GetLectorQuery request, CancellationToken cancellationToken)
+        {
+            var student = await _context.Lectors
+                .SingleOrDefaultAsync(s => s.Id == request.LectorId, cancellationToken);
+
+            return _mapper.Map<LectorVm>(student);
+        }
+    }
+}
