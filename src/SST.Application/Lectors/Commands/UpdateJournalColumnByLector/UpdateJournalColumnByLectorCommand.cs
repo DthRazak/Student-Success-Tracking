1c1550c6cce8a8d2b77ef08374fffb79b7210f27
Calvados0806 <flamaster0807@gmail.com>
2020-04-29 22:34:01 +0300

    added UpdateJournalColumnByLector command

---
+++ src/SST.Application/Lectors/Commands/UpdateJournalColumnByLector/UpdateJournalColumnByLectorCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using MediatR;
+
+namespace SST.Application.Lectors.Commands.UpdateJournalColumnByLector
+{
+    public class UpdateJournalColumnByLectorCommand : IRequest
+    {
+        [Required]
+        public int JournalColumnId { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public string? Note { get; set; }
+
+        public int? GroupSubjectId { get; set; }
+
+        public int LectorId { get; set; }
+    }
+}
