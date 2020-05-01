db27aca4ea72432e2af44a884f1bd0d7675bf879
Володимир Мільчановський <vovamilchan@gmail.com>
2020-05-01 11:54:36 +0300

    Added GetJournalByGroupAndSubjectQuery

---
+++ src/SST.Application/Journal/Queries/GetJournalByGroupAndSubject/GetJournalByGroupAndSubjectQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace SST.Application.Journal.Queries.GetJournalByGroupAndSubject
+{
+    public class GetJournalByGroupAndSubjectQuery : IRequest<JournalVm>
+    {
+        public int GroupId { get; set; }
+
+        public int SubjectId { get; set; }
+    }
+}
