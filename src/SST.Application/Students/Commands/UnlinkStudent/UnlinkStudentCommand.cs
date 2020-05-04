610d959c1d4ad9a3ec6552e4bb6aa1257744230a
Calvados0806 <flamaster0807@gmail.com>
2020-05-04 12:35:58 +0300

    added UnlinkStudent command

---
+++ src/SST.Application/Students/Commands/UnlinkStudent/UnlinkStudentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SST.Application.Students.Commands.UnlinkStudent
+{
+    public class UnlinkStudentCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ src/SST.Application/Students/Commands/UnlinkStudent/UnlinkStudentCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SST.Application.Common.Interfaces;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SST.Application.Students.Commands.UnlinkStudent
+{
+    public class UnlinkStudentCommandHandler : IRequestHandler<UnlinkStudentCommand, Unit>
+    {
+        private readonly ISSTDbContext _context;
+
+        public UnlinkStudentCommandHandler(ISSTDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(UnlinkStudentCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Students
+                .FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            entity.UserRef = null;
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
