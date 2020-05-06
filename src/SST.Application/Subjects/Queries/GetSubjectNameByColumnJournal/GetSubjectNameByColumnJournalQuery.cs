d3893634f980bc06479bbfe44220bb6e783820ff
Володимир Мільчановський <vovamilchan@gmail.com>
2020-05-06 14:43:08 +0300

    Added GetSubjectNameByColumnJournalQuery

---
+++ src/SST.Application/Subjects/Queries/GetSubjectNameByColumnJournal/GetSubjectNameByColumnJournalQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SST.Application.Subjects.Queries.GetSubjectNameByColumnJournal
+{
+    public class GetSubjectNameByColumnJournalQuery : IRequest<SubjectDto>
+    {
+        public int JournalColumnId { get; set; }
+    }
+}
