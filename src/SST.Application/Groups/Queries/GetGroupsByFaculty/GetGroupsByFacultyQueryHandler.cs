da84a125113e7cfe7178f1d0a5b57fcf1330a175
Володимир Мільчановський <vovamilchan@gmail.com>
2020-05-07 16:41:16 +0300

    Added GetGroupsByFacultyQuery

---
+++ src/SST.Application/Groups/Queries/GetGroupsByFaculty/GetGroupsByFacultyQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SST.Application.Common.Interfaces;
+
+namespace SST.Application.Groups.Queries.GetGroupsByFaculty
+{
+    public class GetGroupsByFacultyQueryHandler : IRequestHandler<GetGroupsByFacultyQuery, GroupsListVm>
+    {
+        private readonly ISSTDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetGroupsByFacultyQueryHandler(ISSTDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<GroupsListVm> Handle(GetGroupsByFacultyQuery request, CancellationToken cancellationToken)
+        {
+            var groups = await _context.Groups
+               .Where(g => g.Faculty == request.Faculty)
+               .OrderBy(g => g.Name)
+               .ProjectTo<GroupsDto>(_mapper.ConfigurationProvider)
+               .ToListAsync(cancellationToken);
+
+            return new GroupsListVm { Groups = groups };
+        }
+    }
+}
