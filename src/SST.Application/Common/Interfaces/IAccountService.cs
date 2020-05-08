27f6709d916d6ac0b24a01f6dc79909c5463ac19
Володимир Мільчановський <vovamilchan@gmail.com>
2020-05-08 16:25:29 +0300

    Added methods to change password

--- src/SST.Application/Common/Interfaces/IAccountService.cs
+++ src/SST.Application/Common/Interfaces/IAccountService.cs
@@ -10,5 +10,7 @@ namespace SST.Application.Common.Interfaces
         public Task CreateLectorAccount(string email, string password, int lectorId);
 
         public Task<ClaimsPrincipal> Login(string email, string password);
+
+        public Task ChangePassword(string user, string oldpassword, string password);
     }
 }
